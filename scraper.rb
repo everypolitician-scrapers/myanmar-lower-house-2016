c251ebdf8b3603c0b7c0a3f67203e64ae35789b9
Oliver Denman <oliver@therewillbeanimals.com>
2016-11-03 16:39:25 +0000

    Scrape from English and Burmese json
    Scrapes the Burmese and English json separately then
    pulls English names into the Burmese set.

--- scraper.rb
+++ scraper.rb
@@ -6,8 +6,22 @@ require 'combine_popolo_memberships'
 
 require_relative 'lib/members.rb'
 
-members_url = 'http://api.openhluttaw.org/en/memberships'
-members = Members.new(members_url).to_h[:members_of_the_lower_house]
+members_en_url = 'http://api.openhluttaw.org/en/memberships'
+members_my_url = 'http://api.openhluttaw.org/my/memberships'
+members_en = Members.new(members_en_url).to_h[:members_of_the_lower_house]
+members_my = Members.new(members_my_url).to_h[:members_of_the_lower_house]
+
+members = members_my.map do |member|
+  data = member.to_h
+  member_en = members_en.find do |member_en|
+    member_en.to_h[:id] == data[:id]
+  end
+  data[:name__en] = member_en.to_h[:name]
+  data[:name__my] = data[:name]
+  data[:alternate_names__en] = member_en.to_h[:alternate_names]
+  data[:alternate_names__my] = data[:alternate_names]
+  data
+end
 
 members.each do |member|
   ScraperWiki.save_sqlite([:id], member.to_h)

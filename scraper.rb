e4a34c313bb1f7594c91463860d1746fcd37f74a
Oliver Denman <oliver@therewillbeanimals.com>
2016-11-04 11:05:06 +0000

    Combine English data with Burmese data

--- scraper.rb
+++ scraper.rb
@@ -4,25 +4,20 @@ require 'scraperwiki'
 require 'colorize'
 require 'combine_popolo_memberships'
 
-require_relative 'lib/members.rb'
+require_relative 'lib/member_my.rb'
+require_relative 'lib/member_en.rb'
+require_relative 'lib/members'
 
 members_en_url = 'http://api.openhluttaw.org/en/memberships'
 members_my_url = 'http://api.openhluttaw.org/my/memberships'
-members_en = Members.new(members_en_url).to_h[:members_of_the_lower_house]
-members_my = Members.new(members_my_url).to_h[:members_of_the_lower_house]
+members_en = Members.new(members_en_url, MemberEN).to_h[:members_of_the_lower_house]
+members_my = Members.new(members_my_url, MemberMY).to_h[:members_of_the_lower_house]
 
-members = members_my.map do |member|
-  data = member.to_h
-  member_en = members_en.find do |member_en|
-    member_en.to_h[:id] == data[:id]
+members = members_my.map do |member_my|
+  data_en = members_en.find do |member_en|
+    member_en.to_h[:id] == member_my.to_h[:id]
   end
-  data[:name__en] = member_en.to_h[:name]
-  data[:name__my] = data[:name]
-  data[:alternate_names__en] = member_en.to_h[:alternate_names]
-  data[:alternate_names__my] = data[:alternate_names]
-  data[:party__en] = member_en.to_h[:party]
-  data[:party__my] = data[:party]
-  data
+  member_my.to_h.merge(data_en.to_h)
 end
 
 members.each do |member|

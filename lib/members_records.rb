0e827513159206dd4731ad6fb376d1f29db09d0b
Oliver Denman <oliver@therewillbeanimals.com>
2017-01-10 16:00:28 +0000

    Create MemberRecords as a JSON fragment

--- lib/members_records.rb
+++ lib/members_records.rb
@@ -8,7 +8,7 @@ OpenURI::Cache.cache_path = '.cache'
 class MembersRecords < Scraped::JSON
   field :members_of_the_lower_house do
     lower_house_members.map do |mem|
-      MemberRecords.new(response: response, json: mem)
+      fragment mem => MemberRecords
     end
   end
 

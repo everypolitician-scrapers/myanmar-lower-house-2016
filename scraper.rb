7d509a81e12b39f963b3ee5824786ed66267f6fd
Tony Bowden <tony@tmtm.com>
2017-02-09 08:42:39 +0000

    Clean out database on each run

--- scraper.rb
+++ scraper.rb
@@ -7,8 +7,7 @@ require 'scraperwiki'
 require_relative 'lib/members.rb'
 
 members_url = 'http://api.openhluttaw.org/en/memberships'
-members = Members.new(members_url).to_h[:members_of_the_lower_house]
 
-members.each do |member|
-  ScraperWiki.save_sqlite([:id], member.to_h)
-end
+ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
+data = Members.new(members_url).members_of_the_lower_house.map(&:to_h)
+ScraperWiki.save_sqlite([:id], data)

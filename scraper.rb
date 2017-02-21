47cae138fb4fe75d78dfc47ed02eba6ff8cbbd51
Oliver Denman <oliver@therewillbeanimals.com>
2017-02-21 11:53:01 +0000

    Scrape data

--- scraper.rb
+++ scraper.rb
@@ -3,11 +3,29 @@
 # frozen_string_literal: true
 
 require 'scraperwiki'
+require 'scraped'
+require 'pry'
+require_rel 'lib'
 
-require_relative 'lib/members.rb'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
 
-members_url = 'http://api.openhluttaw.org/en/memberships'
+def scrape(h)
+  url, klass = h.to_a.first
+  klass.new(response: Scraped::Request.new(url: url).response)
+end
 
 ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
-data = Members.new(members_url).members_of_the_lower_house.map(&:to_h)
+
+def scrape_lower_house_member_records(url, data = [])
+  member_records = (scrape url => MemberRecords)
+  data += member_records.members_of_the_lower_house.map(&:to_h)
+
+  return data if (next_page = member_records.next).nil?
+  scrape_lower_house_member_records(next_page, data)
+end
+
+start_url = 'http://api.openhluttaw.org/en/memberships'
+data = scrape_lower_house_member_records(start_url)
+# data.each { |d| puts d.reject { |_k, v| v.to_s.empty? }.sort_by { |k, _v| k }.to_h }
 ScraperWiki.save_sqlite([:id], data)

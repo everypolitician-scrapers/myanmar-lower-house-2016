59feca4b73242b48a6e54e29c12c9452b4206f61
Tony Bowden <tony@tmtm.com>
2017-05-19 08:37:50 +0100

    Show debug output if MORPH_DEBUG is set

--- scraper.rb
+++ scraper.rb
@@ -10,7 +10,8 @@ OpenURI::Cache.cache_path = '.cache'
 # require 'scraped_page_archive/open-uri'
 
 members_url = 'http://api.openhluttaw.org/en/memberships'
+data = Members.new(members_url).members_of_the_lower_house.map(&:to_h)
+data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
 
 ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
-data = Members.new(members_url).members_of_the_lower_house.map(&:to_h)
 ScraperWiki.save_sqlite([:id], data)
